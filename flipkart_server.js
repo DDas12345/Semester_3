91243efa15ae6ab1bef69887cced2cd209a9ba16
Debanshika Das <debanshikadas@Debanshikas-MacBook-Pro.local>
2026-08-27 12:08:30 +0530

    Update Flipkart server

--- flipkart_server.js
+++ flipkart_server.js
@@ -1,8 +1,8 @@
 const http = require('http');
-console.log(http);
+// console.log(http);
 const server = http.createServer((req, response) => {
     response.end('Section E waale bacche padhai nahi karte');
 });
-server.listen(3000, () => {
-    console.log("My Flipkart serverr is live on port 3000");
+server.listen(1, () => {
+    console.log("My Flipkart serverr is live on port 1");
 })
