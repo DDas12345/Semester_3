4db4c694b6376d47261f3b39810bf0d1f5797994
Debanshika Das <debanshikadas@Debanshikas-MacBook-Pro.local>
2026-09-08 11:59:00 +0530

    change

--- express-server.js
+++ express-server.js
@@ -1,21 +1,43 @@
 const express = require("express")
 const app = express();
-app.get('/', (req, res) => {
-    console.log("Home page requested")
-    res.send("Welcome to my Home page")
-});
-app.get('/users', (req, res) => {
-    console.log("Users page requested")
-    res.send("Welcome to my Users page")
-});
-app.get('/about', (req, res) => {
-    console.log("About page requested")
-    res.send("Welcome to my About page")
-});
-app.get('/Ruhi', (req, res) => {
-    console.log("This is my request")
-    res.send("Welcome to my Ruhi page")
-});
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+
+app.use(express.json()) //middleware to parse json data
+
+// function logger(req, res, next) {
+//     console.log(`This is request url ${req.url} and request method is ${req.method}`)
+//     console.log('Class E waale bacche proxy maarte hai.')
+//     next();
+// }
+
+// function signIn (req, res, next) {
+//     // console.log(`This is request url ${req.url} and request method is ${req.method}`)
+//     console.log('SignIn User is called')
+//     next();
+// }
+
+//Register my middleware
+// app.use(logger);
+// app.use(signIn);
+
+// app.get('/', (req, res) => {
+//     console.log("Home page requested")
+//     res.send("Welcome to my Home page")
+// });
+
+app.post("/users", (req, res) => {
+    console.log(req.body)
+    res.send(`Response send ${req.body}`)
 })
+// app.get('/users', signIn, (req, res) => {
+//     console.log("Users page requested")
+//     res.send("Welcome to my Users page")
+// });
+// app.get('/about', (req, res) => {
+//     console.log("About page requested")
+//     res.send("Welcome to my About page")
+// });
+// app.get('/Ruhi', (req, res) => {
+//     console.log("This is my request")
+//     res.send("Welcome to my Ruhi page")
+// });
+app.listen(3000)
