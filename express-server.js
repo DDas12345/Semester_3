0f9447ba1b4cf549b8adcbe04f6bfbdfeb372737
Debanshika Das <debanshikadas@Debanshikas-MacBook-Pro.local>
2026-09-08 11:01:09 +0530

    Describe your changes

--- express-server.js
+++ express-server.js
@@ -1,3 +1,7 @@
 const express = require("express")
 const app = express();
+app.get('/', (req, res) => {
+    console.log("Home page requested")
+    res.send("Welcome to my Home page")
+});
 app.listen(3000)
