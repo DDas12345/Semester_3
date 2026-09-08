36fc45b002a72773d0bebe5c9922c01816160a9f
Debanshika Das <debanshikadas@Debanshikas-MacBook-Pro.local>
2026-09-08 11:03:16 +0530

    Describe your changes

--- express-server.js
+++ express-server.js
@@ -4,4 +4,8 @@ app.get('/', (req, res) => {
     console.log("Home page requested")
     res.send("Welcome to my Home page")
 });
+app.get('/users', (req, res) => {
+    console.log("Users page requested")
+    res.send("Welcome to my Users page")
+});
 app.listen(3000)
