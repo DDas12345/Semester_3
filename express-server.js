cb668fc105a379c1c515c8aa2344c3c2cf5b5f3d
Debanshika Das <debanshikadas@Debanshikas-MacBook-Pro.local>
2026-09-08 11:06:31 +0530

    Express.js

--- express-server.js
+++ express-server.js
@@ -8,4 +8,8 @@ app.get('/users', (req, res) => {
     console.log("Users page requested")
     res.send("Welcome to my Users page")
 });
+app.get('/about', (req, res) => {
+    console.log("About page requested")
+    res.send("Welcome to my About page")
+});
 app.listen(3000)
