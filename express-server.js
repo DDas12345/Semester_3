6765213f46a0ab4e0fea0faadd183d6c6804cf98
Debanshika Das <debanshikadas@Debanshikas-MacBook-Pro.local>
2026-09-10 11:03:03 +0530

    change

--- express-server.js
+++ express-server.js
@@ -9,35 +9,35 @@ app.use(express.json()) //middleware to parse json data
 //     next();
 // }
 
-// function signIn (req, res, next) {
-//     // console.log(`This is request url ${req.url} and request method is ${req.method}`)
-//     console.log('SignIn User is called')
-//     next();
-// }
+function signIn (req, res, next) {
+    // console.log(`This is request url ${req.url} and request method is ${req.method}`)
+    console.log('SignIn User is called')
+    next();
+}
 
 //Register my middleware
 // app.use(logger);
-// app.use(signIn);
+app.use(signIn);
 
-// app.get('/', (req, res) => {
-//     console.log("Home page requested")
-//     res.send("Welcome to my Home page")
-// });
+app.get('/', (req, res) => {
+    console.log("Home page requested")
+    res.send("Welcome to my Home page")
+});
 
-app.post("/users", (req, res) => {
+app.post('/users', signIn, (req, res) => {
     console.log(req.body)
-    res.send(`Response send ${req.body}`)
+    res.send(`Response send to user ${req.body}`)
 })
-// app.get('/users', signIn, (req, res) => {
-//     console.log("Users page requested")
-//     res.send("Welcome to my Users page")
-// });
-// app.get('/about', (req, res) => {
-//     console.log("About page requested")
-//     res.send("Welcome to my About page")
-// });
-// app.get('/Ruhi', (req, res) => {
-//     console.log("This is my request")
-//     res.send("Welcome to my Ruhi page")
-// });
+app.get('/users', signIn, (req, res) => {
+    console.log("Users page requested")
+    res.send("Welcome to my Users page")
+});
+app.get('/about', signIn, (req, res) => {
+    console.log("About page requested")
+    res.send("Welcome to my About page")
+});
+app.get('/Ruhi', signIn, (req, res) => {
+    console.log("This is my request")
+    res.send("Welcome to my Ruhi page")
+});
 app.listen(3000)
